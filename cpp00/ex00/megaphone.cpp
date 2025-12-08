2a05a2dac009ff8a4b639404351497995173bd13
NatimiDev <nati.mikula@gmail.com>
2025-12-08 11:39:18 +0100

    add ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void uppercase(char *str)
+{
+    size_t len = std::strlen(str);
+
+	for (size_t i = 0; i < len; i++)
+	{
+		std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[i])));
+    }
+}
+
+int main(int argc, char **argv)
+{
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	for (int i = 1; i < argc; i++)
+	{
+		uppercase(argv[i]);
+	}
+	return 0;
+}
