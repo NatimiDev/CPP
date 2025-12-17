f3b4a5ccd81a1083a837370c0dd3d74d268a26b5
NatimiDev <nati.mikula@gmail.com>
2025-12-17 14:35:41 +0100

    add new line at the end ot the ouput

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,21 +3,25 @@
 
 void uppercase(char *str)
 {
-    size_t len = std::strlen(str);
+	size_t len = std::strlen(str);
 
 	for (size_t i = 0; i < len; i++)
 	{
 		std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[i])));
-    }
+	}
 }
 
 int main(int argc, char **argv)
 {
 	if (argc <= 1)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
 	for (int i = 1; i < argc; i++)
 	{
 		uppercase(argv[i]);
 	}
+	std::cout << std::endl;
 	return 0;
 }
